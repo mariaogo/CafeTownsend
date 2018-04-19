252226fdb608aa0987017d21733d00e66f721e2c
mariaogo <mogonowska@mobiquityinc.com>
2018-04-19 15:56:51 +0200

    Successful Login

--- src/test/java/net/maria/selenium/login/LoginTest.java
+++ src/test/java/net/maria/selenium/login/LoginTest.java
@@ -40,5 +40,42 @@ public class LoginTest {
 
         final WebElement passwordElement = driver.findElement(By.cssSelector("#login-form > fieldset > label:nth-child(4) > input"));
         passwordElement.sendKeys("Skywalker");
+
+        driver.findElement(By.tagName("button")).click();
+        driver.findElement(By.id("greetings"));
+
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void wrongPasswordTest() {
+        driver.get("http://cafetownsend-angular-rails.herokuapp.com/login");
+
+        (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver d) {
+                return d.findElement(By.cssSelector("#login-form > fieldset > label:nth-child(3) > input")) != null;
+            }
+        });
+
+        final WebElement userNameElement = driver.findElement(By.cssSelector("#login-form > fieldset > label:nth-child(3) > input"));
+        userNameElement.sendKeys("Luke");
+
+        final WebElement passwordElement = driver.findElement(By.cssSelector("#login-form > fieldset > label:nth-child(4) > input"));
+        passwordElement.sendKeys("Milkyway");
+
+        driver.findElement(By.tagName("button")).click();
+
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+
+            driver.findElement(By.id("greetings"));
+        }
+
     }
 }
